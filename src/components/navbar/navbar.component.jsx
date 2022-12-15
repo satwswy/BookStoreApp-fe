4baaa01d8a97eb4cbb9bae349deebf8b122b9334
marios <mgeorgak859@gmail.com>
2022-12-15 16:25:28 +0200

    updates navbar to hamburger menu

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { MdClose } from "react-icons/md"
+import { FiMenu } from "react-icons/fi"
 import {
     NavigationContainer,
     NavLinksContainer,
     LogoContainer,
     NavLink,
+    Button,
+    HamburgerContainer
 } from './navbar.styles.jsx';
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 
@@ -11,6 +16,13 @@ import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+    }
+    const closeMenu = () => {
+        setNavbarOpen(false)
+    }
     return (
         <>
             <NavigationContainer>
@@ -18,11 +30,28 @@ const Navbar = () => {
                     <Logo />
                 </LogoContainer>
                 <NavLinksContainer>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/categories'>Categories</NavLink>
-                    <NavLink to='/auth'>Sign in</NavLink>
+                    <NavLink to='/' onClick={() => closeMenu()}>Home</NavLink>
+                    <NavLink to='/categories' onClick={() => closeMenu()}>Categories</NavLink>
+                    <NavLink to='/auth' onClick={() => closeMenu()}>Sign in</NavLink>
                 </NavLinksContainer>
             </NavigationContainer>
+            <Button onClick={handleToggle}>{navbarOpen ? (
+                    <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+                ) : (
+                    <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
+                )}
+            </Button>
+            { navbarOpen && 
+            <HamburgerContainer>
+                <LogoContainer to='/'>
+                    <Logo />
+                </LogoContainer>
+                <NavLinksContainer>
+                    <NavLink to='/' onClick={() => closeMenu()}>Home</NavLink>
+                    <NavLink to='/categories' onClick={() => closeMenu()}>Categories</NavLink>
+                    <NavLink to='/auth' onClick={() => closeMenu()}>Sign in</NavLink>
+                </NavLinksContainer>
+            </HamburgerContainer>}
             <Outlet />
         </>
     )
--- src/components/navbar/navbar.styles.jsx
+++ src/components/navbar/navbar.styles.jsx
@@ -9,11 +9,29 @@ display: flex;
 justify-content: space-between;
 margin-bottom: 25px;
 align-items: center;
-
+@media (max-width: 700px) {
+   display: none;
+ }
+`
+export const HamburgerContainer = styled.div`
+padding:0;
+height: 100%;
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+margin-bottom: 25px;
+align-items: flex-start;
+gap: 15px;
+margin-top:50px;
+background-color: antiquewhite;
+@media (min-width: 701px) {
+   display: none;
+ }
 `
 export const LogoContainer = styled(Link)`
-   height: 100 %;
-   width: 70px;
+height: 100 %;
+width: 70px;
       
 `
 export const NavLinksContainer = styled.div`
@@ -22,9 +40,23 @@ height: 100 %;
 display: flex;
 justify-content: flex-end;
 gap: 30px;
+@media (max-width: 700px) {
+   flex-direction: column;
+ }
 `
 
 export const NavLink = styled(Link)`
 font-size: 1.8rem;
 cursor: pointer;
+@media (max-width: 700px) {
+   color:black;
+ }
+`
+export const Button = styled.button`
+display: none;
+cursor: pointer;
+border: none;
+@media (max-width: 700px) {
+   display: inline-block;
+ }
 `
