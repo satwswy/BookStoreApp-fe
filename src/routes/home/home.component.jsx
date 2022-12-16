35607651ae2df919da30afcf90021cd03b6fd6b8
marios <mgeorgak859@gmail.com>
2022-12-16 04:59:30 +0200

    updates main page card

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,10 @@
 import { 
 HeroContainer,
 SelectedBookContainer,
+CardContainer,
+CardImage,
+CardTitle,
+CardDesc,
 BookListContainer,
 Searchbox,
 BookList
@@ -9,7 +13,13 @@ BookList
 const Home = () => {
     return (
         <HeroContainer>
-            <SelectedBookContainer></SelectedBookContainer>
+            <SelectedBookContainer>
+                <CardContainer>
+                    <CardImage></CardImage>
+                    <CardTitle>hey</CardTitle>
+                    <CardDesc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi placeat modi, corporis quae dignissimos saepe esse similique optio ad! Quae maiores incidunt minus suscipit tempore cum blanditiis error quo dignissimos.</CardDesc>
+                </CardContainer>
+            </SelectedBookContainer>
             <BookListContainer>
                 <Searchbox></Searchbox>
                 <BookList></BookList>
--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const HeroContainer= styled.div`
+export const HeroContainer = styled.div`
 width: 100%;
 display: flex;
 flex-wrap: wrap;
@@ -8,21 +8,56 @@ border:2px solid black;
 height:90vh;
 `
 
-export const SelectedBookContainer= styled.div`
+export const SelectedBookContainer = styled.div`
 width: 35%;
 height:100%;
-border:1px solid pink;
+border:1px solid yellow;
+`
+export const CardContainer = styled.div`
+width:100%;
+height:100%;
+display:flex;
+flex-direction:column;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+&:hover{
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);   
+}
+`
+export const CardImage = styled.div`
+object-fit: cover;
+width: 100%;
+height: 35%;
+border:2px solid purple;
+`
+export const CardTitle = styled.h2`
+font-size: 1.5rem;
+color: #ffffff;
+font-weight: 600;
+text-align: center;
+text-transform: uppercase;
+letter-spacing: 0.01em;
+border:2px solid blue;
+`
+export const CardDesc = styled.p`
+font-size: 1.2rem;
+color: #ffffff;
+letter-spacing: 0.01em;
+border:2px solid green;
 `
 
-export const BookListContainer= styled.div`
+
+
+
+export const BookListContainer = styled.div`
 width: 65%;
 height:100%;
 border:1px solid blue;
 `
 
-export const Searchbox= styled.div`
+export const Searchbox = styled.div`
 
 `
-export const BookList= styled.div`
+export const BookList = styled.div`
 
 `
