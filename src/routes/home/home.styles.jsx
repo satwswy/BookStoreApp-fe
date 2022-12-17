3b87e93a7b936e382dc41109059023d8be1a0978
marios <mgeorgak859@gmail.com>
2022-12-17 15:35:24 +0200

    Update home.styles.jsx

--- src/routes/home/home.styles.jsx
+++ src/routes/home/home.styles.jsx
@@ -94,3 +94,10 @@ export const Title = styled.span`
 export const PagesContainer = styled.div`
 display:flex;
 `
+export const Button = styled.button`
+font-size: 1em;
+margin: 1em;
+padding: 0.25em 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+`;
