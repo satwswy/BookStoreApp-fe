4da6a565e0fadebe189d3c11f90f15b48f0adadd
marios <mgeorgak859@gmail.com>
2022-12-18 23:13:26 +0200

    Update book-list.styles.jsx

--- src/components/book-list/book-list.styles.jsx
+++ src/components/book-list/book-list.styles.jsx
@@ -23,18 +23,22 @@ column-gap: 30px;
 
 export const Book = styled.div`
 width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap:5%;
-  height: 350px;
-  align-items: center;
-  margin-bottom:25px;
-  img {
+display: flex;
+flex-direction: column;
+gap:5%;
+height: 350px;
+align-items: center;
+margin-bottom:25px;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+border:1px dotted white;
+img {
     width: 100%;
     height: 75%;
     object-fit: cover;
   }
-  &:hover {
+&:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     img {
       opacity: 0.8;
     }
@@ -44,6 +48,9 @@ width: 100%;
 export const Title = styled.span`
   width: 100%;
   height:20%;
+  color: white;
+  text-align:center;
+  font-size:1.2rem;
 `;
 
 
@@ -55,8 +62,7 @@ export const Button = styled.button`
 font-size: 1em;
 margin: 1em;
 padding: 0.25em 1em;
-border: 2px solid palevioletred;
-border-radius: 3px;
+border:none;
 &:hover{
   cursor:pointer;
 }
