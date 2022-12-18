c5f8aa8a9b7067aeb026beaf0e055a888a81d064
marios <mgeorgak859@gmail.com>
2022-12-18 22:28:04 +0200

    Update selected-book.component.jsx

--- src/components/selectedbook/selected-book.component.jsx
+++ src/components/selectedbook/selected-book.component.jsx
@@ -1,19 +1,20 @@
-import { SelectedBookContainer,
+import {
+    SelectedBookContainer,
     CardContainer,
     CardImage,
     CardTitle,
     CardDesc
 } from "./selected-book.styles"
 
-const SelectedBook = ()=>{
-    return(
+const SelectedBook = () => {
+    return (
         <SelectedBookContainer>
-                <CardContainer>
-                    <CardImage></CardImage>
-                    <CardTitle>hey</CardTitle>
-                    <CardDesc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi placeat modi, corporis quae dignissimos saepe esse similique optio ad! Quae maiores incidunt minus suscipit tempore cum blanditiis error quo dignissimos.</CardDesc>
-                </CardContainer>
-            </SelectedBookContainer>
+            <CardContainer>
+                <CardImage></CardImage>
+                <CardTitle>hey</CardTitle>
+                <CardDesc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi placeat modi, corporis quae dignissimos saepe esse similique optio ad! Quae maiores incidunt minus suscipit tempore cum blanditiis error quo dignissimos.</CardDesc>
+            </CardContainer>
+        </SelectedBookContainer>
     )
 }
 
