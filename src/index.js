8bc6026d6c05cfe281c8bbbfe8c0e364f3788a4f
marios <mgeorgak859@gmail.com>
2022-12-20 13:51:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { SelectedBookProvider } from "./context/SelectedBookContext";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SelectedBookProvider>
+        <App />
+      </SelectedBookProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
