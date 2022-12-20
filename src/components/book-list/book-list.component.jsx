ad1e824c292fa925069e5533f5c1f019d2bc4443
marios <mgeorgak859@gmail.com>
2022-12-20 14:36:33 +0200

    Update book-list.component.jsx

--- src/components/book-list/book-list.component.jsx
+++ src/components/book-list/book-list.component.jsx
@@ -8,9 +8,11 @@ import {
 } from "./book-list.styles"
 import books from '../../books.json'
 import SearchBox from "../search-box/search-box.component";
-import { useState } from "react";
+import { useState,useContext } from "react";
+import { SelectedBookContext } from "../../context/SelectedBookContext";
 
 const BookList = () => {
+    const {setSelectedBook}=useContext(SelectedBookContext)
     const [currentPage, setCurrentPage] = useState(1);
     const [booksPerPage] = useState(12);
     const pageNumbers = [];
@@ -26,7 +28,7 @@ const BookList = () => {
             <SearchBox/>
             <Books>
                 {currentBooks.map((book, index) => {
-                    return <Book key={index}>
+                    return <Book key={index} onClick={()=>{setSelectedBook(book)}}>
                         <img src={book.image} alt={book.title} />
                         <Title>{book.title}</Title>
                     </Book>
